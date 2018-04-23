1d9a891ad9eb90b925e5d9c3422ab7b2ee4ca835
Gubatenko Artem <ingco@mail.ru>
2018-04-23 20:07:03 +0300

    Объединил контракт токена ERC20 и его распродажу в один контракт

--- build/flattened/CryptohomaToken.sol
+++ build/flattened/CryptohomaToken.sol
@@ -7,6 +7,55 @@ contract ERC20Basic {
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 contract BasicToken is ERC20Basic {
   using SafeMath for uint256;
 
@@ -69,53 +118,39 @@ contract BurnableToken is BasicToken {
   }
 }
 
-library SafeMath {
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
 
   /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
   }
 
   /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
 
   /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public view returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract StandardToken is ERC20, BasicToken {
@@ -206,41 +241,6 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
-contract Ownable {
-  address public owner;
-
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
-}
-
 contract MintableToken is StandardToken, Ownable {
   event Mint(address indexed to, uint256 amount);
   event MintFinished();
@@ -280,9 +280,348 @@ contract MintableToken is StandardToken, Ownable {
 
 contract CryptohomaToken is StandardToken, MintableToken, BurnableToken {
     
-    string public name = "CryptoHoma3";
-    string public symbol = "HOMA3";
+    string public name = "CryptohomaToken";
+    string public symbol = "HOMA";
     uint public decimals = 18;
 
+    using SafeMath for uint256;
+
+    // Amount of wei raised
+    uint256 public weiRaised;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    _processPurchase(_beneficiary, tokens);
+    emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+
+    _forwardFunds();
+  }
+
+    // Начало Crowdsale
+    uint start = 1523782800;
+
+    // Период сбора денег
+    uint period = 31;
+
+    // Адрес получателя средств, переданных за токены
+    address public multisig = 0x16Dc1a72B44997A5e169874f901570Bc687a2ee0;
+
+    // Всего будет выпущено
+    uint256 public totalSupply = 50000000 * 1 ether;
+
+    // Всего токенов выпустили
+    uint256 public totalMinted;
+
+    // Процент на Pre-Sale = 3.125
+    uint256 public presale_tokens = 1562500 * 1 ether;
+    // Процент на Рекламу
+    uint public bounty_percent = 5;
+    // Процент на AirDrop
+    uint public airdrop_percent = 2;
+    // Процент организаторов
+    uint public organizers_percent = 15;
+
+    address public presale = 0x1e363e5AF2bA28C40abf56d6D0f6Fd91445d6bAB;
+    address public bounty = 0x5FEc0d2733b28e89d4DF7A052B95160E324e2274;
+    address public airdrop = 0xcbe9df774b77eCb64C6E695BB9731d9a42a6dE7a;
+    // Кошельки организаторов
+    address public organizer1 = 0x77F5F6b39e00f0fBaa4D943d65983D17ed7e0fa4;
+    address public organizer2 = 0x90e0A14d75ECD5f301be8F9e2081faB12BB8ADB2;
+    address public organizer3 = 0x7Cc3E141B41ee40904dD293C150281925daED9d6;
+    address public organizer4 = 0x56c2c7A7391e17399aa7A03e03463fa1f5EB693a;
+    address public organizer5 = 0x571bC2033F7b495F6630bc3c429649c0af417469;
+    address public organizer6 = 0xE7DA66b24212a08e6bBb1abe93b8D1Ff35E37C41;
+    address public organizer7 = 0x26B038B6199f81dF008e2664a36a3e842b0895d6;
+    address public organizer8 = 0x76A4dA392034418CEe3e79c28f47F982b1522677;
+    // Стоимость токена в wei
+    uint256 public rate = 0.000011 * 1 ether;
+    uint256 public rate2 = 0.000015 * 1 ether;
+
+    function TestTokenH6() public {
+
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(presale_tokens);
+        // Выпускаем токены на адрес
+        super.mint(presale, presale_tokens);
+
+        // Отправляем токены на bounty
+        uint256 tokens = totalSupply.mul(bounty_percent).div(100);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+        // Выпускаем токены на адрес
+        super.mint(bounty, tokens);
+
+        // Отправляем на Air Drop
+        tokens = totalSupply.mul(airdrop_percent).div(100);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+        // Выпускаем токены на адрес
+        super.mint(airdrop, tokens);
+
+        // Токены для организаторов
+        tokens = totalSupply.mul(organizers_percent).div(100);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+        // берем 8-ую часть и переправлем организаторам
+        tokens = tokens.div(8);
+        // Выпускаем токены на адреса
+        super.mint(organizer1, tokens);
+        super.mint(organizer2, tokens);
+        super.mint(organizer3, tokens);
+        super.mint(organizer4, tokens);
+        super.mint(organizer5, tokens);
+        super.mint(organizer6, tokens);
+        super.mint(organizer7, tokens);
+        super.mint(organizer8, tokens);
+
+    }
+
+    /**
+    * @dev Override to extend the way in which ether is converted to tokens. ***ПЕРЕОПРЕДЕЛЕНО***
+    * @param _weiAmount Value in wei to be converted into tokens
+    * @return Number of tokens that can be purchased with the specified _weiAmount
+    */
+    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+        return _weiAmount / rate * 1 ether;
+    }
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use super to concatenate validations. ***ПЕРЕОПРЕДЕЛЕНО***
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+        require(_beneficiary != address(0));
+        require(_weiAmount != 0);
+
+        require(now > start && now < start + period * 1 days);
+
+        // Если с начала распродаж прошло 14 дней, то повышаем цену на токен
+        if (now > start.add(15 * 1 days)) {
+            rate = rate2;
+        }
+
+        // calculate token amount to be created
+        uint256 tokens = _getTokenAmount(_weiAmount);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+
+        // Выпущенных токенов должно быть меньше общего кол-ва
+        require(totalSupply >= totalMinted);
+
+    }
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    super.transfer(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    multisig.transfer(msg.value);
+  }
+
+}
+
+contract Crowdsale {
+  using SafeMath for uint256;
+
+  // The token being sold
+  ERC20 public token;
+
+  // Address where funds are collected
+  address public wallet;
+
+  // How many token units a buyer gets per wei
+  uint256 public rate;
+
+  // Amount of wei raised
+  uint256 public weiRaised;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+  /**
+   * @param _rate Number of token units a buyer gets per wei
+   * @param _wallet Address where collected funds will be forwarded to
+   * @param _token Address of the token being sold
+   */
+  function Crowdsale(uint256 _rate, address _wallet, ERC20 _token) public {
+    require(_rate > 0);
+    require(_wallet != address(0));
+    require(_token != address(0));
+
+    rate = _rate;
+    wallet = _wallet;
+    token = _token;
+  }
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    _processPurchase(_beneficiary, tokens);
+    emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+
+    _updatePurchasingState(_beneficiary, weiAmount);
+
+    _forwardFunds();
+    _postValidatePurchase(_beneficiary, weiAmount);
+  }
+
+  // -----------------------------------------
+  // Internal interface (extensible)
+  // -----------------------------------------
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use super to concatenate validations.
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0);
+  }
+
+  /**
+   * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _postValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    // optional override
+  }
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    token.transfer(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Override for extensions that require an internal state to check for validity (current user contributions, etc.)
+   * @param _beneficiary Address receiving the tokens
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _updatePurchasingState(address _beneficiary, uint256 _weiAmount) internal {
+    // optional override
+  }
+
+  /**
+   * @dev Override to extend the way in which ether is converted to tokens.
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    return _weiAmount.mul(rate);
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+}
+
+contract MintedCrowdsale is Crowdsale {
+
+  /**
+   * @dev Overrides delivery by minting tokens upon purchase.
+   * @param _beneficiary Token purchaser
+   * @param _tokenAmount Number of tokens to be minted
+   */
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    require(MintableToken(token).mint(_beneficiary, _tokenAmount));
+  }
 }
 
--- contracts/CryptohomaToken.sol
+++ contracts/CryptohomaToken.sol
@@ -1,16 +1,203 @@
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 
 /**
- * Контракт CryptoHoma
+ * Контракт TestToken
  */
 contract CryptohomaToken is StandardToken, MintableToken, BurnableToken {
     
-    string public name = "CryptoHoma3";
-    string public symbol = "HOMA3";
+    string public name = "CryptohomaToken";
+    string public symbol = "HOMA";
     uint public decimals = 18;
+
+    using SafeMath for uint256;
+
+    // Amount of wei raised
+    uint256 public weiRaised;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    _processPurchase(_beneficiary, tokens);
+    emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+
+    _forwardFunds();
+  }
+
+    // Начало Crowdsale
+    uint start = 1523782800;
+
+    // Период сбора денег
+    uint period = 31;
+
+    // Адрес получателя средств, переданных за токены
+    address public multisig = 0x16Dc1a72B44997A5e169874f901570Bc687a2ee0;
+
+    // Всего будет выпущено
+    uint256 public totalSupply = 50000000 * 1 ether;
+
+    // Всего токенов выпустили
+    uint256 public totalMinted;
+
+    // Процент на Pre-Sale = 3.125
+    uint256 public presale_tokens = 1562500 * 1 ether;
+    // Процент на Рекламу
+    uint public bounty_percent = 5;
+    // Процент на AirDrop
+    uint public airdrop_percent = 2;
+    // Процент организаторов
+    uint public organizers_percent = 15;
+
+    address public presale = 0x1e363e5AF2bA28C40abf56d6D0f6Fd91445d6bAB;
+    address public bounty = 0x5FEc0d2733b28e89d4DF7A052B95160E324e2274;
+    address public airdrop = 0xcbe9df774b77eCb64C6E695BB9731d9a42a6dE7a;
+    // Кошельки организаторов
+    address public organizer1 = 0x77F5F6b39e00f0fBaa4D943d65983D17ed7e0fa4;
+    address public organizer2 = 0x90e0A14d75ECD5f301be8F9e2081faB12BB8ADB2;
+    address public organizer3 = 0x7Cc3E141B41ee40904dD293C150281925daED9d6;
+    address public organizer4 = 0x56c2c7A7391e17399aa7A03e03463fa1f5EB693a;
+    address public organizer5 = 0x571bC2033F7b495F6630bc3c429649c0af417469;
+    address public organizer6 = 0xE7DA66b24212a08e6bBb1abe93b8D1Ff35E37C41;
+    address public organizer7 = 0x26B038B6199f81dF008e2664a36a3e842b0895d6;
+    address public organizer8 = 0x76A4dA392034418CEe3e79c28f47F982b1522677;
+    // Стоимость токена в wei
+    uint256 public rate = 0.000011 * 1 ether;
+    uint256 public rate2 = 0.000015 * 1 ether;
+
+    function TestTokenH6() public {
+
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(presale_tokens);
+        // Выпускаем токены на адрес
+        super.mint(presale, presale_tokens);
+
+        // Отправляем токены на bounty
+        uint256 tokens = totalSupply.mul(bounty_percent).div(100);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+        // Выпускаем токены на адрес
+        super.mint(bounty, tokens);
+
+        // Отправляем на Air Drop
+        tokens = totalSupply.mul(airdrop_percent).div(100);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+        // Выпускаем токены на адрес
+        super.mint(airdrop, tokens);
+
+        // Токены для организаторов
+        tokens = totalSupply.mul(organizers_percent).div(100);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+        // берем 8-ую часть и переправлем организаторам
+        tokens = tokens.div(8);
+        // Выпускаем токены на адреса
+        super.mint(organizer1, tokens);
+        super.mint(organizer2, tokens);
+        super.mint(organizer3, tokens);
+        super.mint(organizer4, tokens);
+        super.mint(organizer5, tokens);
+        super.mint(organizer6, tokens);
+        super.mint(organizer7, tokens);
+        super.mint(organizer8, tokens);
+
+    }
+
+    /**
+    * @dev Override to extend the way in which ether is converted to tokens. ***ПЕРЕОПРЕДЕЛЕНО***
+    * @param _weiAmount Value in wei to be converted into tokens
+    * @return Number of tokens that can be purchased with the specified _weiAmount
+    */
+    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+        return _weiAmount / rate * 1 ether;
+    }
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use super to concatenate validations. ***ПЕРЕОПРЕДЕЛЕНО***
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+        require(_beneficiary != address(0));
+        require(_weiAmount != 0);
+
+        require(now > start && now < start + period * 1 days);
+
+        // Если с начала распродаж прошло 14 дней, то повышаем цену на токен
+        if (now > start.add(15 * 1 days)) {
+            rate = rate2;
+        }
+
+        // calculate token amount to be created
+        uint256 tokens = _getTokenAmount(_weiAmount);
+        // Считаем выпущенные токены
+        totalMinted = totalMinted.add(tokens);
+
+        // Выпущенных токенов должно быть меньше общего кол-ва
+        require(totalSupply >= totalMinted);
+
+    }
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    super.transfer(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    multisig.transfer(msg.value);
+  }
 
 }
